64a7519269de5345a1d8c38bcb9a58ab4c4ef02e
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-19 12:23:51 +0300

    added a header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import  Header  from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello world
+      <Header></Header>
     </div>
   );
 }
