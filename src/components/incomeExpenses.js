c83afa4d20cfa31334cf524d9c7d7c00ae622aa0
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-20 16:47:30 +0300

    added a delete transaction function

--- src/components/incomeExpenses.js
+++ src/components/incomeExpenses.js
@@ -1,20 +1,19 @@
 import React,{ useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
-const IncomeExpenses = () => {
+export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
 
   const amounts = transactions.map(transaction => transaction.amount);
 
   const income = amounts
     .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+    .reduce((acc, item) => (acc += item), 0);
 
   const expense = (
     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
-  ).toFixed(2);
+  );
 
   return (
       <div className="inc-exp-container">
@@ -30,6 +29,3 @@ const IncomeExpenses = () => {
   )
 }
 
-export  {
-  IncomeExpenses
-};
