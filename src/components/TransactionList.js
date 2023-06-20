724f7bd1a9a7c9d3c1c21d2a369ad59b57a4e041
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-20 12:47:31 +0300

    styled border for the transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import  Transaction  from './Transaction';
+
 import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
@@ -8,9 +10,7 @@ const { transactions } = useContext(GlobalContext);
     <div>
        <h3>History</h3>
       <ul className="list">
-        { transactions.map(transaction =>(<li className="minus">
-          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
-        </li> ))}
+        { transactions.map(transaction =>(<Transaction key={transaction.id} transaction={ transaction}/> ))}
       </ul>
     </div>
   )
