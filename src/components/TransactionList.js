71206ed19e43964635da6d6fd853a38e20a16ceb
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-20 12:23:49 +0300

    created a transaction file in the components folder

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -9,11 +9,8 @@ const { transactions } = useContext(GlobalContext);
        <h3>History</h3>
       <ul className="list">
         { transactions.map(transaction =>(<li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
+          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
         </li> ))}
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li> 
       </ul>
     </div>
   )
