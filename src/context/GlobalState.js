5a0c054a6ba434189e6eb04daccd373144885333
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-20 11:54:13 +0300

    added a provider component

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, { createContext,useReducer } from "react";
+import AppReducer from './AppReducer';
 
 //Initial state
 const initialState = {
@@ -10,5 +11,16 @@ const initialState = {
     ]
 }
 
-//craete context
+//create context
 export const GlobalContext = createContext(initialState);
+
+//Provider component
+export const GlobalProvider = ( { children}) => {
+    const [state, dispatch] =useReducer (AppReducer, initialState);
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        { children }
+    </GlobalContext.Provider>);
+}
