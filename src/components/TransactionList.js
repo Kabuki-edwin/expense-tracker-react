9d54bcf5fee9afa58918dcd4f882dbff0c2605d6
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-20 12:20:26 +0300

    adding items in the transaction list file

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+const { transactions } = useContext(GlobalContext);
+
   return (
     <div>
        <h3>History</h3>
       <ul className="list">
+        { transactions.map(transaction =>(<li className="minus">
+          Cash <span>-$400</span><button className="delete-btn">x</button>
+        </li> ))}
         <li className="minus">
           Cash <span>-$400</span><button className="delete-btn">x</button>
         </li> 
