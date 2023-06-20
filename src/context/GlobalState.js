0f29e5e050b738a0ae05c9dac9eeeadb99acc623
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-20 11:46:21 +0300

    added a context folder with a globalstate file

---
+++ src/context/GlobalState.js
@@ -0,0 +1,14 @@
+import React, { createContext,useReducer } from "react";
+
+//Initial state
+const initialState = {
+    transactions : [
+   { id: 1, text: 'Flower', amount: -20 },
+   { id: 2, text: 'Salary', amount: 300 },
+   { id: 3, text: 'Book', amount: -10 },
+   { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+//craete context
+export const GlobalContext = createContext(initialState);
