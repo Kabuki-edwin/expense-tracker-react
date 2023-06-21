5f1a236a0cccc7bad72dc59655010b4dad500fbd
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-21 10:09:56 +0300

    creating the add transaction function

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -5,7 +5,12 @@ export default (state, action) => {
                 ...state,
                 transactions: state.transactions.filter(transaction =>transaction.id !== action.payload)
             }
-        default:
+            case 'ADD_TRANSACTION' :
+                return {
+                    ...state,
+                    transactions:[action.payload, ...state.transactions]
+                }
+        default: 
             return state;
     }
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -26,9 +26,17 @@ export const GlobalProvider = ( { children}) => {
         });
     }
 
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload:transaction
+        });
+    }
+
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
     }}>
         { children }
     </GlobalContext.Provider>);
